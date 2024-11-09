b6cff1a08ea6a3f9a4f682420b8636ced3894818
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-11-09 12:19:50 +0600

    feat: import port value from .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AlbumModule } from './album/album.module';
 import { ArtistModule } from './artist/artist.module';
 import { FavsModule } from './favs/favs.module';
@@ -6,7 +7,14 @@ import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, ArtistModule, AlbumModule, TrackModule, FavsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UserModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavsModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<string>('PORT') ?? 4000;
+
+  await app.listen(port);
 }
 bootstrap();
