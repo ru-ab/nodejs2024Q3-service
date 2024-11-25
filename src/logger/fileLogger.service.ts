e4dfa4cae0bb1e36ac1add7d8d947e990d7620db
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-11-25 23:29:12 +0600

    fix: change log files delimiter

--- src/logger/fileLogger.service.ts
+++ src/logger/fileLogger.service.ts
@@ -92,6 +92,6 @@ export class FileLoggerService {
     const minutes = String(date.getMinutes()).padStart(2, '0');
     const seconds = String(date.getSeconds()).padStart(2, '0');
 
-    return `${year}-${month}-${day}_${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
   }
 }
