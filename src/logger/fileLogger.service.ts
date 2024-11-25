abaa243cd843efaf647c1780ba4c241a1daad532
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-11-25 22:02:38 +0600

    feat: add file logs rotation

--- src/logger/fileLogger.service.ts
+++ src/logger/fileLogger.service.ts
@@ -1,48 +1,97 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Stats } from 'node:fs';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 
+const DEFAULT_LOG_MAX_FILE_SIZE = 1024 * 1024;
+
 @Injectable()
 export class FileLoggerService {
-  private appLogPath: string;
-  private errorsLogPath: string;
+  private logsFolderPath: string;
+  private appLogFileName: string;
+  private errorsLogFileName: string;
+  private maxFileSizeInKB: number;
 
-  constructor() {
-    const logsFolderPath = path.join(__dirname, '..', '..', 'logs');
-    this.appLogPath = path.join(logsFolderPath, 'app.log');
-    this.errorsLogPath = path.join(logsFolderPath, 'errors.log');
+  constructor(private readonly configService: ConfigService) {
+    this.logsFolderPath = path.join(__dirname, '..', '..', 'logs');
+    this.appLogFileName = 'app.log';
+    this.errorsLogFileName = 'errors.log';
 
-    fs.mkdir(logsFolderPath, { recursive: true });
+    const maxFileSize = parseInt(
+      this.configService.getOrThrow<string>('LOG_MAX_FILE_SIZE'),
+    );
+
+    this.maxFileSizeInKB = Number.isInteger(maxFileSize)
+      ? maxFileSize * 1024
+      : DEFAULT_LOG_MAX_FILE_SIZE;
+
+    fs.mkdir(this.logsFolderPath, { recursive: true });
   }
 
   async debug(message: string) {
-    await this.write(this.appLogPath, message);
+    await this.write(this.appLogFileName, message);
   }
 
   async verbose(message: string) {
-    await this.write(this.appLogPath, message);
+    await this.write(this.appLogFileName, message);
   }
 
   async log(message: string) {
-    await this.write(this.appLogPath, message);
+    await this.write(this.appLogFileName, message);
   }
 
   async warn(message: string) {
-    await this.write(this.appLogPath, message);
+    await this.write(this.appLogFileName, message);
   }
 
   async error(message: string) {
-    await this.write(this.appLogPath, message);
-    await this.write(this.errorsLogPath, message);
+    await this.write(this.appLogFileName, message);
+    await this.write(this.errorsLogFileName, message);
   }
 
-  private async write(filePath: string, content: string) {
+  private async write(fileName: string, content: string) {
     try {
+      const filePath = path.join(this.logsFolderPath, fileName);
+      await this.rotateLog(fileName);
       await fs.appendFile(filePath, content, { flag: 'a+' });
     } catch (err) {
       if (err instanceof Error) {
         console.log(`Could not write to log file: ${err.message}`);
       }
     }
+  }
+
+  private async rotateLog(fileName: string) {
+    try {
+      const filePath = path.join(this.logsFolderPath, fileName);
+      const stats: Stats | null = await fs.stat(filePath).catch(() => null);
+      if (stats && stats.size >= this.maxFileSizeInKB) {
+        await this.rotateLogFile(fileName);
+      }
+    } catch {}
+  }
+
+  private async rotateLogFile(fileName: string) {
+    try {
+      const now = new Date();
+      const dateString = this.formatDateToString(now);
+      const rotatedFileName = `${fileName}.${dateString}`;
+      const rotatedFilePath = path.join(this.logsFolderPath, rotatedFileName);
+      const originalFilePath = path.join(this.logsFolderPath, fileName);
+
+      await fs.rename(originalFilePath, rotatedFilePath);
+    } catch {}
+  }
+
+  private formatDateToString(date: Date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}_${hours}:${minutes}:${seconds}`;
   }
 }
--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -21,69 +21,102 @@ export class LoggerServiceImp implements LoggerService {
     }
   }
 
-  debug?(message: any, ...optionalParams: any[]) {
+  debug?(message: unknown, ...optionalParams: unknown[]) {
     const enableIfLevel = ['debug'];
     if (!enableIfLevel.includes(this.logLevel)) {
       return;
     }
 
-    const messageText = `[DEBUG] [${optionalParams[0]}] ${message}\n`;
-    process.stdout.write(colorize('blue', messageText));
+    const messageText = this.createMessageText(
+      'DEBUG',
+      message,
+      optionalParams,
+    );
+    process.stdout.write(this.colorize('blue', messageText));
     this.fileLogger.debug(messageText);
   }
 
-  verbose(message: any, ...optionalParams: any[]) {
+  verbose(message: unknown, ...optionalParams: unknown[]) {
     const enableIfLevel = ['verbose', 'debug'];
     if (!enableIfLevel.includes(this.logLevel)) {
       return;
     }
 
-    const messageText = `[VERBOSE] [${optionalParams[0]}] ${message}\n`;
+    const messageText = this.createMessageText(
+      'VERBOSE',
+      message,
+      optionalParams,
+    );
     process.stdout.write(messageText);
     this.fileLogger.verbose(messageText);
   }
 
-  log(message: any, ...optionalParams: any[]) {
+  log(message: unknown, ...optionalParams: unknown[]) {
     const enableIfLevel = ['log', 'verbose', 'debug'];
     if (!enableIfLevel.includes(this.logLevel)) {
       return;
     }
 
-    const messageText = `[LOG] [${optionalParams[0]}] ${message}\n`;
-    process.stdout.write(colorize('green', messageText));
+    const messageText = this.createMessageText('LOG', message, optionalParams);
+    process.stdout.write(this.colorize('green', messageText));
     this.fileLogger.log(messageText);
   }
 
-  warn(message: any, ...optionalParams: any[]) {
+  warn(message: unknown, ...optionalParams: unknown[]) {
     const enableIfLevel = ['warn', 'log', 'verbose', 'debug'];
     if (!enableIfLevel.includes(this.logLevel)) {
       return;
     }
 
-    const messageText = `[WARN] [${optionalParams[0]}] ${message}\n`;
-    process.stdout.write(colorize('yellow', messageText));
+    const messageText = this.createMessageText('WARN', message, optionalParams);
+    process.stdout.write(this.colorize('yellow', messageText));
     this.fileLogger.warn(messageText);
   }
 
-  error(message: any, ...optionalParams: any[]) {
+  error(message: unknown, ...optionalParams: unknown[]) {
     const enableIfLevel = ['error', 'warn', 'log', 'verbose', 'debug'];
     if (!enableIfLevel.includes(this.logLevel)) {
       return;
     }
 
-    const messageText = `[ERROR] [${optionalParams[0]}] ${message}\n`;
-    process.stdout.write(colorize('red', messageText));
+    const messageText = this.createMessageText(
+      'ERROR',
+      message,
+      optionalParams,
+    );
+    process.stdout.write(this.colorize('red', messageText));
     this.fileLogger.error(messageText);
   }
-}
 
-function colorize(color: 'red' | 'yellow' | 'green' | 'blue', output: string) {
-  const colors = {
-    red: '91',
-    green: '92',
-    yellow: '93',
-    blue: '94',
-  };
+  private createMessageText(
+    logLevel: string,
+    message: unknown,
+    optionalParams: unknown[],
+  ) {
+    const now = new Date();
+    const timestamp = this.formatDateToString(now);
+    return `${timestamp} - [${logLevel}] [${optionalParams[0]}] ${message}\n`;
+  }
 
-  return ['\x1b[', colors[color], 'm', output, '\x1b[0m'].join('');
+  private colorize(color: 'red' | 'yellow' | 'green' | 'blue', output: string) {
+    const colors = {
+      red: '91',
+      green: '92',
+      yellow: '93',
+      blue: '94',
+    };
+
+    return ['\x1b[', colors[color], 'm', output, '\x1b[0m'].join('');
+  }
+
+  private formatDateToString(date: Date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
 }
